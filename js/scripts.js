ae02c79225482ed1152ba84272579f4705aac3ca
PiresTiago <tiao.silva.pires11@gmail.com>
2019-04-01 02:12:57 +0100

    1.0

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function displayMenu(id) {
+    document.getElementById("menu").style.display = "block";
+    document.getElementById(id).style.display = "none";
+  }
+
+  function hideMenu(id) {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById(id).style.display = "block";
+  }
