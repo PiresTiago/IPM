d73f7bea47dbe5ddf08cdcc6a1b72290a7c39c48
PiresTiago <tiao.silva.pires11@gmail.com>
2019-05-14 03:49:48 +0100

    working MyWeb

--- js/scripts.js
+++ js/scripts.js
@@ -327,45 +327,89 @@ function loadLocal() {
     document.getElementById("Buttons").appendChild(link);
   }
 }
-
-function like(id) {
-
-  if (document.getElementById(id).className == "fa-2x fas fa-heart text-danger") {
-    document.getElementById(id).className = "fa-2x far fa-heart";
-
-  }
-  else if (document.getElementById(id).className == "fa-2x far fa-heart") {
-    document.getElementById(id).className = "fa-2x fas fa-heart text-danger";
+function loadMyWeb(PeopleData) {
+  localStorage.setItem("PeopleData", JSON.stringify(PeopleData))
+  for (let i = 0; i < PeopleData.length; i++) {
+    for (let j = 0; j < PeopleData[i].posts.length; j++) {
+      localStorage.setItem(PeopleData[i].posts[j].imgSrc, "false")
+    }
   }
 }
 
-function likeProfile(id) {
+function like(id) {
 
-  if (document.getElementById(id + "1").className == "fa-2x fas fa-heart text-danger") {
+  var likeState = localStorage.getItem(id)
+  console.log(likeState)
+
+  if (likeState == "true") {
+    likeState = "false";
     document.getElementById(id).removeChild(document.getElementById(id).firstChild)
     var icoLike = document.createElement('i')
     icoLike.id = id + "1"
     icoLike.className = "fa-2x far fa-heart"
     document.getElementById(id).appendChild(icoLike)
+    console.log(likeState)
   }
-  else if (document.getElementById(id + "1").className == "fa-2x far fa-heart") {
+  else {
+    likeState = "true"
     document.getElementById(id).removeChild(document.getElementById(id).firstChild)
     var icoLike = document.createElement('i')
     icoLike.id = id + "1"
     icoLike.className = "fa-2x fas fa-heart text-danger"
     document.getElementById(id).appendChild(icoLike)
+    console.log(likeState)
+  }
+  localStorage.setItem(id, likeState)
+}
+
+function displayComments(Post) {
+  document.getElementById('commentsContainer').style = "background-color: #F4F4F4;position: absolute;top: 54%;left: 50%;margin-top: -158px; margin-left: -122px;width: 6.5cm;height: 6.5cm;  z-index: 10;display: block;"
+  for (let i = 0; i < Post.comments.length; i++) {
+    var name = document.createElement('p'),
+      text = document.createElement('p'),
+      divider = document.createElement('div');
+
+    name.innerText = Post.comments[i].name
+    name.className = "font-weight-bold ml-2 m-0 p-0"
+    divider.className = "dropdown-divider border mt-2 mb-2 m-0 p-0"
+    text.innerText = Post.comments[i].comment;
+    text.className = "ml-3  m-0 p-0"
+    document.getElementById('commentsContainer').appendChild(name);
+    document.getElementById('commentsContainer').appendChild(text);
+    document.getElementById('commentsContainer').appendChild(divider);
   }
 
 }
-function toProfile(PersonData) {
-  localStorage.setItem("PersonData", JSON.stringify(PersonData));
+
+function hideComments() {
+  while (document.getElementById('commentsContainer').childNodes.length > 2) {
+    document.getElementById('commentsContainer').removeChild(document.getElementById('commentsContainer').lastChild);
+
+    document.getElementById('commentsContainer').style = "display:none"
+  }
+}
+
+function toNearWeb(PeopleData) {
+  localStorage.setItem("PeopleData", JSON.stringify(PeopleData));
 }
 
 function toFeed(PeopleData) {
   localStorage.setItem("PeopleData", JSON.stringify(PeopleData))
 }
 
+function toProfile(PersonData) {
+  localStorage.setItem("PersonData", JSON.stringify(PersonData));
+}
+
+
 function loadProfile() {
+
+  var previousPage = localStorage.getItem("return");
+  localStorage.removeItem("return");
+  if (previousPage != null) {
+    if (previousPage == "nearWeb.html")
+      document.getElementById("return").href = "nearWeb.html"
+  }
 
   var PersonData = JSON.parse(localStorage.getItem("PersonData"))
   localStorage.removeItem("PersonData");
@@ -390,7 +434,7 @@ function loadProfile() {
       buttonLike = document.createElement('button'),
       buttonComments = document.createElement('button'),
       icoLike = document.createElement('i'),
-      icoComments = document.createElement('i')
+      icoComments = document.createElement('i');
 
     divContainer.className = "row justify-content-center mb-4"
     divCard.className = "card col-10 m-0"
@@ -404,8 +448,8 @@ function loadProfile() {
     imgProfile.src = PersonData.imgProfile
 
     spanName.innerText = PersonData.name
-    spanName.style = "font-size:15px"
-
+    spanName.style = "font-size:17px;"
+    spanTime.style = "font-size:17px;"
     spanTime.innerText = PersonData.posts[i].timeOfPost
 
 
@@ -419,14 +463,21 @@ function loadProfile() {
     buttonComments.className = "btn float-right p-0 m-0"
     buttonComments.id = "comments"
 
+
     buttonLike.addEventListener("click", function () {
-      likeProfile(PersonData.posts[i].imgSrc)
+      like(PersonData.posts[i].imgSrc)
     })
-    icoComments.addEventListener("click", function () {
-      readcomments(this.id)
+    buttonComments.addEventListener("click", function () {
+      displayComments(PersonData.posts[i])
     })
 
-    icoLike.className = "fa-2x far fa-heart"
+    //CHECK STATE OF LIKE
+    var likeState = localStorage.getItem(PersonData.posts[i].imgSrc)
+    if (likeState == "true")
+      icoLike.className = "fa-2x fas fa-heart text-danger"
+    else
+      icoLike.className = "fa-2x far fa-heart"
+
     icoLike.id = PersonData.posts[i].imgSrc + "1"
     icoComments.className = "fa-2x far fa-comment-dots"
 
@@ -463,7 +514,9 @@ function loadFeed() {
       divCol1 = document.createElement('div'),
       divCol2 = document.createElement('div'),
       divCol3 = document.createElement('div'),
+      linkimg = document.createElement('a'),
       imgProfile = document.createElement('img'),
+      linkName = document.createElement('a'),
       spanName = document.createElement('span'),
       spanTime = document.createElement('span'),
       imgPost = document.createElement('img'),
@@ -481,13 +534,15 @@ function loadFeed() {
     divCol2.className = "col-7 mt-2 m-0 p-0 text-left"
     divCol3.className = "col-2 mt-2 m-0 p-0 text-center text-muted"
 
+    linkimg.href = "profile.html"
     imgProfile.className = "rounded-circle mt-1 mb-1 p-0"
     imgProfile.style = "width:35px"
     imgProfile.src = PeopleData[i].imgProfile
 
+    linkName.href = "profile.html"
     spanName.innerText = PeopleData[i].name
-    spanName.style = "font-size:15px"
-
+    spanName.style = "font-size:17px;"
+    spanTime.style = "font-size:17px;"
     spanTime.innerText = PeopleData[i].posts[0].timeOfPost
 
 
@@ -501,23 +556,41 @@ function loadFeed() {
     buttonComments.className = "btn float-right p-0 m-0"
     buttonComments.id = "comments"
 
-    buttonLike.addEventListener("click", function () {
-      likeProfile(PeopleData[i].posts[0].imgSrc)
-    })
-    icoComments.addEventListener("click", function () {
-      readcomments(this.id)
+
+    linkimg.addEventListener("click", function () {
+      toProfile(getPerson(PeopleData[i].name))
     })
 
-    icoLike.className = "fa-2x far fa-heart"
+    linkName.addEventListener("click", function () {
+      toProfile(getPerson(PeopleData[i].name))
+    })
+
+    buttonLike.addEventListener("click", function () {
+      like(PeopleData[i].posts[0].imgSrc)
+    })
+    buttonComments.addEventListener("click", function () {
+      displayComments(PeopleData[i].posts[0])
+    })
+
+
+
+    //CHECK STATE OF LIKE
+    var likeState = localStorage.getItem(PeopleData[i].posts[0].imgSrc)
+    if (likeState == "true")
+      icoLike.className = "fa-2x fas fa-heart text-danger"
+    else
+      icoLike.className = "fa-2x far fa-heart"
     icoLike.id = PeopleData[i].posts[0].imgSrc + "1"
     icoComments.className = "fa-2x far fa-comment-dots"
 
     divContainer.appendChild(divCard)
     divCard.appendChild(divRow)
     divRow.appendChild(divCol1)
-    divCol1.appendChild(imgProfile)
+    divCol1.appendChild(linkimg)
+    linkimg.appendChild(imgProfile)
     divRow.appendChild(divCol2)
-    divCol2.appendChild(spanName)
+    divCol2.appendChild(linkName)
+    linkName.appendChild(spanName)
     divRow.appendChild(divCol3)
     divCol3.appendChild(spanTime)
 
@@ -530,6 +603,109 @@ function loadFeed() {
     buttonComments.appendChild(icoComments)
     document.getElementById('FeedContainer').appendChild(divContainer)
   }
+}
+
+function loadNearWeb() {
+
+  var PeopleData = JSON.parse(localStorage.getItem("PeopleData"))
+  localStorage.removeItem("PeopleData");
+  for (let i = 0; i < PeopleData.length; i++)
+    for (let j = 0; j < PeopleData[i].posts.length; j++) {
+      console.log(PeopleData[i].posts[j].location)
+      if (PeopleData[i].posts[j].location == "Lisboa") {
+        var divContainer = document.createElement('div'),
+          divCard = document.createElement('div'),
+          divRow = document.createElement('div'),
+          divCol1 = document.createElement('div'),
+          divCol2 = document.createElement('div'),
+          divCol3 = document.createElement('div'),
+          linkimg = document.createElement('a'),
+          imgProfile = document.createElement('img'),
+          linkName = document.createElement('a'),
+          spanName = document.createElement('span'),
+          spanTime = document.createElement('span'),
+          imgPost = document.createElement('img'),
+          location = document.createElement('p'),
+          divBody = document.createElement('div'),
+          buttonLike = document.createElement('button'),
+          buttonComments = document.createElement('button'),
+          icoLike = document.createElement('i'),
+          icoComments = document.createElement('i')
+
+        divContainer.className = "row justify-content-center mb-4"
+        divCard.className = "card col-10 m-0"
+        divRow.className = "row justify-content-center"
+        divCol1.className = "col-3 m-0 p-0 text-center"
+        divCol2.className = "col-7 mt-2 m-0 p-0 text-left"
+        divCol3.className = "col-2 mt-2 m-0 p-0 text-center text-muted"
+
+        linkimg.href = "profile.html"
+        imgProfile.className = "rounded-circle mt-1 mb-1 p-0"
+        imgProfile.style = "width:35px"
+        imgProfile.src = PeopleData[i].imgProfile
+
+        linkName.href = "profile.html"
+        spanName.innerText = PeopleData[i].name
+        spanName.style = "font-size:17px;"
+        spanTime.style = "font-size:17px;"
+        spanTime.innerText = PeopleData[i].posts[j].timeOfPost
+
+        imgPost.className = "card-img-top"
+        imgPost.src = PeopleData[i].posts[j].imgSrc
+        location.className = "mt-1 font-weight-bold"
+        location.innerText = PeopleData[i].posts[j].location
+        divBody.className = "card-body mt-1 m-0 p-1"
+        buttonLike.className = "btn float-left p-0 m-0"
+        buttonLike.id = PeopleData[i].posts[j].imgSrc
+        buttonComments.className = "btn float-right p-0 m-0"
+        buttonComments.id = "comments"
+
+
+        linkimg.addEventListener("click", function () {
+          toProfile(getPerson(PeopleData[i].name))
+        })
+
+        linkName.addEventListener("click", function () {
+          toProfile(getPerson(PeopleData[i].name))
+        })
+
+        buttonLike.addEventListener("click", function () {
+          like(PeopleData[i].posts[j].imgSrc)
+        })
+        buttonComments.addEventListener("click", function () {
+          displayComments(PeopleData[i].posts[j])
+        })
+
+        //CHECK STATE OF LIKE
+        var likeState = localStorage.getItem(PeopleData[i].posts[j].imgSrc)
+        if (likeState == "true")
+          icoLike.className = "fa-2x fas fa-heart text-danger"
+        else
+          icoLike.className = "fa-2x far fa-heart"
+        icoLike.id = PeopleData[i].posts[j].imgSrc + "1"
+        icoComments.className = "fa-2x far fa-comment-dots"
+
+        divContainer.appendChild(divCard)
+        divCard.appendChild(divRow)
+        divRow.appendChild(divCol1)
+        divCol1.appendChild(linkimg)
+        linkimg.appendChild(imgProfile)
+        divRow.appendChild(divCol2)
+        divCol2.appendChild(linkName)
+        linkName.appendChild(spanName)
+        divRow.appendChild(divCol3)
+        divCol3.appendChild(spanTime)
+
+        divCard.appendChild(imgPost)
+        divCard.appendChild(location)
+        divCard.appendChild(divBody)
+        divBody.appendChild(buttonLike)
+        buttonLike.appendChild(icoLike)
+        divBody.appendChild(buttonComments)
+        buttonComments.appendChild(icoComments)
+        document.getElementById('nearWebContainer').appendChild(divContainer)
+      }
+    }
 }
 
 
--- json/peopleMyWeb.js
+++ json/peopleMyWeb.js
@@ -2,35 +2,26 @@ var peopledata = {
 
     people: [
         {
-            "name": "Jorge Mendes",
-            "age": "53 anos",
-            "imgProfile": "imagens/PeopleMyWeb/jorgeMendes/profile.jpg",
+            "name": "Eder Lopes",
+            "age": "31 anos",
+            "imgProfile": "imagens/PeopleMyWeb/ederLopes/profile.jpg",
             "posts": [
                 {
-                    "imgSrc": "imagens/PeopleMyWeb/jorgeMendes/1.jpeg",
-                    "timeOfPost": "4h",
-                    "location": "Budapeste",
+                    "imgSrc": "imagens/PeopleMyWeb/ederLopes/1.jpg",
+                    "timeOfPost": "1h",
+                    "location": "Stade de France",
+
                     "comments": [
                         {
-                            "name": "Vanessa",
-                            "comment": "WOW!"
+                            "name": "Armando",
+                            "comment": "Que louco!"
                         },
                         {
                             "name": "Daniel",
-                            "comment": "FIXE!"
+                            "comment": "E FOI O ÉDER QUE OS *****!"
                         },
                     ]
                 },
-                {
-                    "imgSrc": "imagens/PeopleMyWeb/jorgeMendes/2.jpg",
-                    "timeOfPost": "6h",
-                    "location": "Budapeste"
-                },
-                {
-                    "imgSrc": "imagens/PeopleMyWeb/jorgeMendes/3.jpeg",
-                    "timeOfPost": "1s",
-                    "location": "Budapeste"
-                }
             ],
         },
         {
@@ -42,6 +33,7 @@ var peopledata = {
                     "imgSrc": "imagens/PeopleMyWeb/joeBerardo/1.jpg",
                     "timeOfPost": "1h",
                     "location": "Porto",
+
                     "comments": [
                         {
                             "name": "Armando",
@@ -53,102 +45,29 @@ var peopledata = {
                         },
                     ]
                 },
-                {
-                    "imgSrc": "imagens/PeopleMyWeb/joeBerardo/2.jpg",
-                    "timeOfPost": "1d",
-                    "location": "Lisboa"
-                },
-                {
-                    "imgSrc": "imagens/PeopleMyWeb/joeBerardo/3.jpg",
-                    "timeOfPost": "2d",
-                    "location": "Lisboa"
-                }
             ],
         },
         {
-            "name": "José Castelo Branco",
-            "age": "56 anos",
-            "imgProfile": "imagens/PeopleMyWeb/joseCasteloBranco/profile.jpg",
+            "name": "Jorge Mendes",
+            "age": "53 anos",
+            "imgProfile": "imagens/PeopleMyWeb/jorgeMendes/profile.jpg",
             "posts": [
                 {
-                    "imgSrc": "imagens/PeopleMyWeb/joseCasteloBranco/1.jpeg",
-                    "timeOfPost": "1h",
-                    "location": "Porto",
+                    "imgSrc": "imagens/PeopleMyWeb/jorgeMendes/1.jpg",
+                    "timeOfPost": "4h",
+                    "location": "Lisboa",
+
                     "comments": [
                         {
-                            "name": "Armando",
-                            "comment": "Que louco!"
+                            "name": "Vanessa",
+                            "comment": "Giros!"
                         },
                         {
                             "name": "Daniel",
-                            "comment": "FIXE!"
+                            "comment": "Grande Jorge!"
                         },
                     ]
                 },
-                {
-                    "imgSrc": "imagens/PeopleMyWeb/joseCasteloBranco/2.jpg",
-                    "timeOfPost": "1d",
-                    "location": "Lisboa"
-                }
-            ],
-        },
-        {
-            "name": "Eder Lopes",
-            "age": "31 anos",
-            "imgProfile": "imagens/PeopleMyWeb/ederLopes/profile.jpg",
-            "posts": [
-                {
-                    "imgSrc": "imagens/PeopleMyWeb/ederLopes/1.jpg",
-                    "timeOfPost": "1h",
-                    "location": "Porto",
-                    "comments": [
-                        {
-                            "name": "Armando",
-                            "comment": "Que louco!"
-                        },
-                        {
-                            "name": "Daniel",
-                            "comment": "FIXE!"
-                        },
-                    ]
-                },
-                {
-                    "imgSrc": "imagens/PeopleMyWeb/ederLopes/2.jpeg",
-                    "timeOfPost": "1d",
-                    "location": "Lisboa"
-                },
-                {
-                    "imgSrc": "imagens/PeopleMyWeb/ederLopes/3.jpg",
-                    "timeOfPost": "2d",
-                    "location": "Lisboa"
-                }
-            ],
-        },
-        {
-            "name": "Duarte Lima",
-            "age": "63 anos",
-            "imgProfile": "imagens/PeopleMyWeb/duarteLima/profile.jpeg",
-            "posts": [
-                {
-                    "imgSrc": "imagens/PeopleMyWeb/duarteLima/1.jpg",
-                    "timeOfPost": "5d",
-                    "location": "Budapeste"
-                },
-                {
-                    "imgSrc": "imagens/PeopleMyWeb/duarteLima/2.jpeg",
-                    "timeOfPost": "1h",
-                    "location": "Budapeste"
-                },
-                {
-                    "imgSrc": "imagens/PeopleMyWeb/duarteLima/3.jpg",
-                    "timeOfPost": "24m",
-                    "location": "Budapeste"
-                },
-                {
-                    "imgSrc": "imagens/PeopleMyWeb/duarteLima/4.jpg",
-                    "timeOfPost": "6s",
-                    "location": "Budapeste"
-                }
             ],
         },
         {
@@ -159,17 +78,20 @@ var peopledata = {
                 {
                     "imgSrc": "imagens/PeopleMyWeb/mariaCaetano/1.jpg",
                     "timeOfPost": "3h",
-                    "location": "Paris"
+                    "location": "Paris",
+
                 },
                 {
                     "imgSrc": "imagens/PeopleMyWeb/mariaCaetano/2.jpg",
                     "timeOfPost": "7d",
-                    "location": "Bélgica"
+                    "location": "Bélgica",
+
                 },
                 {
                     "imgSrc": "imagens/PeopleMyWeb/mariaCaetano/3.jpg",
                     "timeOfPost": "15d",
-                    "location": "Paris"
+                    "location": "Paris",
+
                 }
             ],
         },
@@ -181,17 +103,20 @@ var peopledata = {
                 {
                     "imgSrc": "imagens/PeopleMyWeb/rebecaPhillips/1.jpg",
                     "timeOfPost": "1d",
-                    "location": "Dubai"
+                    "location": "Dubai",
+
                 },
                 {
                     "imgSrc": "imagens/PeopleMyWeb/rebecaPhillips/2.jpg",
                     "timeOfPost": "7d",
-                    "location": "Índia"
+                    "location": "Índia",
+
                 },
                 {
                     "imgSrc": "imagens/PeopleMyWeb/rebecaPhillips/3.jpg",
                     "timeOfPost": "15d",
-                    "location": "Singapura"
+                    "location": "Singapura",
+
                 }
             ],
         },
@@ -203,34 +128,39 @@ var peopledata = {
                 {
                     "imgSrc": "imagens/PeopleMyWeb/johnRonson/1.jpg",
                     "timeOfPost": "3h",
-                    "location": "Las Vegas"
+                    "location": "Las Vegas",
+
                 },
                 {
                     "imgSrc": "imagens/PeopleMyWeb/johnRonson/2.jpg",
                     "timeOfPost": "5d",
-                    "location": "Grand Canyon"
+                    "location": "Grand Canyon",
+
                 },
                 {
                     "imgSrc": "imagens/PeopleMyWeb/johnRonson/3.jpg",
                     "timeOfPost": "19d",
-                    "location": "New York"
+                    "location": "New York",
+
                 }
             ],
         },
         {
             "name": "Joana Silva",
             "age": "22 anos",
-            "imgProfile": "imagens/PeopleMyWeb/joanaSilva/profile.",
+            "imgProfile": "imagens/PeopleMyWeb/joanaSilva/profile.jpg",
             "posts": [
                 {
                     "imgSrc": "imagens/PeopleMyWeb/joanaSilva/1.jpg",
                     "timeOfPost": "5d",
-                    "location": "Coimbra"
+                    "location": "Coimbra",
+
                 },
                 {
                     "imgSrc": "imagens/PeopleMyWeb/joanaSilva/2.jpg",
                     "timeOfPost": "1h",
-                    "location": "Monte Everest"
+                    "location": "Monte Everest",
+
                 },
             ],
         },
@@ -242,7 +172,8 @@ var peopledata = {
                 {
                     "imgSrc": "imagens/PeopleMyWeb/teresaCoutim/1.jpg",
                     "timeOfPost": "1h",
-                    "location": "Lisboa"
+                    "location": "Lisboa",
+
                 },
             ],
         },
@@ -254,17 +185,20 @@ var peopledata = {
                 {
                     "imgSrc": "imagens/PeopleMyWeb/ruiFigueiredo/1.jpg",
                     "timeOfPost": "7d",
-                    "location": "Lisboa"
+                    "location": "Lisboa",
+
                 },
                 {
                     "imgSrc": "imagens/PeopleMyWeb/ruiFigueiredo/2.jpg",
                     "timeOfPost": "5anos",
-                    "location": "Paris"
+                    "location": "Paris",
+
                 },
                 {
                     "imgSrc": "imagens/PeopleMyWeb/ruiFigueiredo/3.jpg",
                     "timeOfPost": "2anos",
-                    "location": "Estrasburgo"
+                    "location": "Estrasburgo",
+
                 }
             ],
         }
